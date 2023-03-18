b75402f36a72cf01c10613c418f94838d2dccf74
Eric Terrisse <eterrissebordas@gmail.com>
2023-03-18 00:08:42 -0400

    setup app with firebase
    Configuration of the app with firebase

---
+++ homework-solver/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Chat from "./screens/Chat";
+
+const Stack = createStackNavigator();
+
+function ChatStack
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+}
