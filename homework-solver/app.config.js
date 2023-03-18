8ce7dfff364e0747b47302be2b7320f0ea622758
Eric Terrisse <eterrissebordas@gmail.com>
2023-03-18 11:28:45 -0400

    chat functionality

--- homework-solver/app.config.js
+++ homework-solver/app.config.js
@@ -32,7 +32,7 @@ export default {
       projectId: process.env.PROJECT_ID,
       storageBucket: process.env.STORAGE_BUCKET,
       messagingSenderId: process.env.MESSAGING_SENDER_ID,
-      appId: process.env.APPID,
+      appId: process.env.APP_ID,
     }
   },
 };
